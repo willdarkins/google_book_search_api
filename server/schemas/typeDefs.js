f36751f03cac91c0e2852af75046a1dc6d91e553
Will Darkins <willdarkins@gmail.com>
2022-01-01 15:44:08 -0800

    created Book type and Auth type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -23,6 +23,23 @@ type Input {
   link: String
 }
 
+type User {
+  _id: ID!
+  username: String!
+  email: String
+  bookCount: Int
+  savedBooks: [Book]
+}
+
+type Book {
+  bookId: ID!
+  authors: [String]
+  description: String
+  image: String
+  link: String
+  title: String!
+}
+
 `;
 
 //export typeDefs
