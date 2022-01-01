b572194a662c7c1ed45c37110b6668fc4572f794
Will Darkins <willdarkins@gmail.com>
2022-01-01 14:28:06 -0800

    added Auth type

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,7 +2,23 @@
 const { gql } = require('apollo-server-express');
 
 //typeDefs
-const typeDefs = gql``;
+const typeDefs = gql`
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Books]
+  }
+
+type Book {
+    bookId: String  
+}
+type Auth {
+  token: ID!
+  user: User
+}
+`;
 
 //export typeDefs
 module.exports = typeDefs;
