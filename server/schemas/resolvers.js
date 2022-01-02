6397218f05ec8da26af2a153a0dc0a1477f42880
Will Darkins <willdarkins@gmail.com>
2022-01-01 16:54:14 -0800

    created first Query for the resolvers of me... added the AthenticationError import and omitted the Mongoose-specific ID + user password

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,19 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User } = require('../models');
 
-
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+          if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            //omit Mongoose-specific __v property and user's password
+            .select('-__v -password');
+            return userData;
+          }
     
+          throw new AuthenticationError('Not logged in');
+        },
+      },
   };
   
   module.exports = resolvers;
