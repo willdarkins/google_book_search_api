151aa2e88fdc1540ed7f108b171c443780c26bcc
Will Darkins <willdarkins@gmail.com>
2022-01-01 20:47:03 -0800

    added LOGIN_USER variable to represent loginUser mutation using Apollo Server

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import gql from 'graphql-tag';
 
+//executes loginUser mutation set up using Apollo Server
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
