c6b19b33abcc112a8b460aec9f6132a52d4d5c98
Will Darkins <willdarkins@gmail.com>
2022-01-01 22:07:52 -0800

    replaced loginUser() functionality with LOGIN_USER mutation funcationality... hopefully this works!

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -2,10 +2,13 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+// import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 
 const LoginForm = () => {
+  const [login] = useMutation(LOGIN_USER)
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
@@ -26,17 +29,11 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await login({
+        variables: { ...userFormData },
+      });
+      Auth.login(data.login.token);
     } catch (err) {
-      console.error(err);
       setShowAlert(true);
     }
 
